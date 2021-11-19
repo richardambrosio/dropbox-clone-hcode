d360abe13f85e5585b7bfba29114fc82ba4ec057
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-19 13:55:51 -0300

    Abrindo o arquivo

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -101,7 +101,7 @@ class DropBoxController {
 
             folderRef.on('value', snapshot => {
                 folderRef.off('value');
-                
+
                 snapshot.forEach(item => {
 
                     let data = item.val();
@@ -423,7 +423,7 @@ class DropBoxController {
                     this.openFolder();
                 break;
                 default:
-                    window.open('/file?path=' + file.filepath);
+                    window.open(file.path);
             }
         });
 
