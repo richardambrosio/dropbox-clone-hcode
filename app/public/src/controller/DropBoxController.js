4463378780a645040c0754ec69a92dd961a0e05c
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-19 13:54:16 -0300

    Excluindo Diretório Firebase

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -61,21 +61,90 @@ class DropBoxController {
             let key = li.dataset.key;
 
             promises.push(new Promise((resolve, reject) => {
-                let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
-                fileRef.delete().then(() => {
-                    resolve({
-                        fields:{
-                            key
-                        }
+
+                if(file.type === 'folder') {
+
+                    this.removeFolderTask(this.currentFolder.join('/'), file.name).then(() => {
+
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+    
                     });
-                }).catch(err =>{
-                    reject(err);
-                });
+
+                } else if (file.type) {
+                    this.removeFile(this.currentFolder.join('/'), file.name).then(() => {
+    
+                        resolve({
+                            fields:{
+                                key
+                            }
+                        });
+    
+                    });
+                }
+
             }));
 
         });
         
         return Promise.all(promises);
+    }
+
+    removeFolderTask(ref, name) {
+
+        return new Promise((resolve, reject) => {
+
+            let folderRef = this.getFirebaseRef(ref + '/' + name);
+
+            folderRef.on('value', snapshot => {
+                folderRef.off('value');
+                
+                snapshot.forEach(item => {
+
+                    let data = item.val();
+                    data.key = item.key;
+
+                    if (data.type === 'folder') {
+                        this.removeFolderTask(ref + '/' + name, data.name).then(() => {
+
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    } else if (data.type) {
+                        this.removeFile(ref + '/' + name, data.name).then(() => {
+
+                            resolve({
+                                fields: {
+                                    key: data.key
+                                }
+                            });
+
+                        }).catch(err => {
+                            reject(err);
+                        });
+                    }
+                });
+
+                folderRef.remove();
+                
+            });
+
+        });
+
+    }
+
+    removeFile(ref, name) {
+        let fileRef = firebase.storage().ref(ref).child(name);
+        return fileRef.delete();
     }
 
     initEvents() {
