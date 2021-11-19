f5b9c4c5319cd838893ec3e86437fae2722b8937
Richard Ambrosio <richard.ambrosio1@live.com>
2021-11-19 12:11:37 -0300

    excluindo arquivos do Firebase Storage

--- app/public/src/controller/DropBoxController.js
+++ app/public/src/controller/DropBoxController.js
@@ -60,11 +60,18 @@ class DropBoxController {
             let file = JSON.parse(li.dataset.file);
             let key = li.dataset.key;
 
-            let formData = new FormData();
-            formData.append('path', file.filepath);
-            formData.append('key', key);
-
-            promises.push(this.ajax('/file', 'DELETE', formData));
+            promises.push(new Promise((resolve, reject) => {
+                let fileRef = firebase.storage().ref(this.currentFolder.join('/')).child(file.name);
+                fileRef.delete().then(() => {
+                    resolve({
+                        fields:{
+                            key
+                        }
+                    });
+                }).catch(err =>{
+                    reject(err);
+                });
+            }));
 
         });
         
